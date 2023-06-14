2bf6dba7f73a666753c2f62eaaca633ad6fa9106
Andrew Minkin <andrew.minkin@percona.com>
2023-06-14 21:03:14 +0600

    EVEREST-105 Finished with examples

--- api-tests/tests/pxc-clusters.spec.ts
+++ api-tests/tests/pxc-clusters.spec.ts
@@ -105,7 +105,7 @@ test('expose cluster after creation', async({ request, page }) => {
   await request.post(`/kubernetes/${kubernetesId}/database-clusters`, {
     data: pxcPayload
   });
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(6000);
 
   let pxcCluster = await request.get(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
   expect (pxcCluster.ok()).toBeTruthy();
@@ -133,6 +133,81 @@ test('expose cluster after creation', async({ request, page }) => {
   expected = (await pxcCluster.json());
 
   expect((await updatedPXCCluster.json()).spec.loadBalancer.type).toBe("LoadBalancer");
+
+  await request.delete(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+
+  pxcCluster = await request.get(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  expect(pxcCluster.status()).toBe(404);
+
+
+});
+test('expose cluster on EKS to the public internet and scale up', async({ request, page }) => {
+  const clusterName = 'eks-pxc-cluster';
+  let pxcPayload =  {
+    apiVersion: "dbaas.percona.com/v1",
+    kind: "DatabaseCluster",
+    metadata: {
+      "name": clusterName,
+      "finalizers": [ "delete-pxc-pvc" ] // Required for the CI/CD workflows. For the end user we should keep unset, unless she set it explicitly
+    },
+    spec: {
+      databaseType: "pxc",
+      databaseImage: "percona/percona-xtradb-cluster:8.0.23-14.1",
+      databaseConfig: "",
+      secretsName: "test-pxc-cluster-secrets",
+      clusterSize: 3,
+      loadBalancer: {
+        type: "haproxy", // HAProxy is the default option. However using proxySQL is available
+        exposeType: "LoadBalancer", // database cluster is exposed
+        size: 3, // Usually, a cluster size is equals to a load balancer set of nodes and any database nodes
+        image: "percona/percona-xtradb-cluster-operator:1.12.0-haproxy",
+        annotations: {
+          // Options below needs to be allied for exposed cluster on AWS infra
+          "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type": "ip",
+          "service.beta.kubernetes.io/aws-load-balancer-scheme": "internet-facing",
+          "service.beta.kubernetes.io/aws-load-balancer-target-group-attributes": "preserve_client_ip.enabled=true",
+          // This setting is required if the cluster needs to be exposed to the public internet (e.g internet facing)
+          "service.beta.kubernetes.io/aws-load-balancer-type": "external"
+        }
+      },
+      dbInstance: {
+        cpu: "1",
+        memory: "1G",
+        diskSize: "15G"
+      }
+    }
+  }
+  await request.post(`/kubernetes/${kubernetesId}/database-clusters`, {
+    data: pxcPayload
+  });
+  await page.waitForTimeout(7000);
+
+  let pxcCluster = await request.get(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  expect (pxcCluster.ok()).toBeTruthy();
+
+  let expected = (await pxcCluster.json());
+
+  expect(expected.metadata.name).toBe(clusterName);
+  expect(expected.spec).toMatchObject(pxcPayload.spec);
+  expect(expected.status.size).toBe(6);
+
+  pxcPayload = expected
+  delete pxcPayload["status"]
+
+  pxcPayload.spec.clusterSize = 5
+  delete pxcPayload.metadata['finalizers']
+
+  // Update PXC cluster
+
+  let updatedPXCCluster = await request.put(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`, {data: pxcPayload});
+  expect(updatedPXCCluster.ok()).toBeTruthy();
+
+  pxcCluster = await request.get(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  expect (pxcCluster.ok()).toBeTruthy();
+
+  expected = (await pxcCluster.json());
+
+  expect((await updatedPXCCluster.json()).status.size).toBe(8);
 
   await request.delete(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
 
