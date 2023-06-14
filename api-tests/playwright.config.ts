0098c77873f1162f46bfc0b313498d5d9b7ac32e
Andrew Minkin <andrew.minkin@percona.com>
2023-06-14 13:21:51 +0600

    EVEREST-105 PXC test

--- api-tests/playwright.config.ts
+++ api-tests/playwright.config.ts
@@ -25,6 +25,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://127.0.0.1:8081',
+    extraHTTPHeaders: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -37,15 +41,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ api-tests/tests/pxc-clusters.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+const kubernetesId = "a0761de5-3ea8-4269-8d18-f2456c0167de";
+
+test('create/edit/delete single node cluster', async({ request }) => {
+  const clusterName = 'test-pxc-cluster';
+  const pxcCluster = await request.post(`/kubernetes/${kubernetesId}/database-clusters`, {
+    data: {
+      apiVersion: "dbaas.percona.com/v1",
+      kind: "DatabaseCluster",
+      metadata: {
+        "name": clusterName,
+        "finalizers": [ "delete-pxc-pvc" ] // Required for the CI/CD workflows. For the end user we should keep unset, unless she set it explicitly
+      },
+      spec: {
+        databaseType: "pxc",
+        databaseImage: "percona/percona-xtradb-cluster:8.0.23-14.1",
+        databaseConfig: "",
+        secretsName: "test-pxc-cluster-secrets",
+        clusterSize: 1,
+        loadBalancer: {
+          type: "haproxy", // HAProxy is the default option. However using proxySQL is available
+          exposeType: "ClusterIP", // database cluster is not exposed by default
+          size: 1, // Usually, a cluster size is equals to a load balancer set of nodes and any database nodes
+          image: "percona/percona-xtradb-cluster-operator:1.12.0-haproxy",
+        },
+        dbInstance: {
+          cpu: "1",
+          memory: "1G",
+          diskSize: "15G"
+        }
+      }
+    }
+  });
+  //expect(pxcCluster.ok()).toBeTruthy();
+   setTimeout(()=>{}, 1000);
+
+  const createdPXCCluster = await request.get(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  //expect (createdPXCCluster.ok()).toBeTruthy();
+  const expected = (await createdPXCCluster.json());
+  expect(expected.metadata.name).toBe("test-pxc-cluster");
+  expect(expected.status.size).toBe(2);
+});
