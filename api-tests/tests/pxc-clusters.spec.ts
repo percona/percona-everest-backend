8c2082921c3065f07468912fd22ea4d28af06f9b
Andrew Minkin <andrew.minkin@percona.com>
2023-06-14 20:20:43 +0600

    EVEREST-105 finished with editing and deleting cluster

--- api-tests/tests/pxc-clusters.spec.ts
+++ api-tests/tests/pxc-clusters.spec.ts
@@ -40,7 +40,7 @@ test('create/edit/delete single node cluster', async({ request, page }) => {
 
   let expected = (await pxcCluster.json());
 
-  expect(expected.metadata.name).toBe("test-pxc-cluster");
+  expect(expected.metadata.name).toBe(clusterName);
   expect(expected.spec).toMatchObject(pxcPayload.spec);
   expect(expected.status.size).toBe(2);
 
@@ -65,6 +65,74 @@ test('create/edit/delete single node cluster', async({ request, page }) => {
   expected = (await pxcCluster.json());
 
   expect((await updatedPXCCluster.json()).spec.databaseConfig).toBe(pxcPayload.spec.databaseConfig);
+
+  await request.delete(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+
+  pxcCluster = await request.get(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  expect(pxcCluster.status()).toBe(404);
+
+
+});
+
+test('expose cluster after creation', async({ request, page }) => {
+  const clusterName = 'exposed-pxc-cluster';
+  let pxcPayload =  {
+    apiVersion: "dbaas.percona.com/v1",
+    kind: "DatabaseCluster",
+    metadata: {
+      "name": clusterName,
+      "finalizers": [ "delete-pxc-pvc" ] // Required for the CI/CD workflows. For the end user we should keep unset, unless she set it explicitly
+    },
+    spec: {
+      databaseType: "pxc",
+      databaseImage: "percona/percona-xtradb-cluster:8.0.23-14.1",
+      databaseConfig: "",
+      secretsName: "test-pxc-cluster-secrets",
+      clusterSize: 3,
+      loadBalancer: {
+        type: "haproxy", // HAProxy is the default option. However using proxySQL is available
+        exposeType: "ClusterIP", // database cluster is not exposed by default
+        size: 3, // Usually, a cluster size is equals to a load balancer set of nodes and any database nodes
+        image: "percona/percona-xtradb-cluster-operator:1.12.0-haproxy",
+      },
+      dbInstance: {
+        cpu: "1",
+        memory: "1G",
+        diskSize: "15G"
+      }
+    }
+  }
+  await request.post(`/kubernetes/${kubernetesId}/database-clusters`, {
+    data: pxcPayload
+  });
+  await page.waitForTimeout(5000);
+
+  let pxcCluster = await request.get(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  expect (pxcCluster.ok()).toBeTruthy();
+
+  let expected = (await pxcCluster.json());
+
+  expect(expected.metadata.name).toBe(clusterName);
+  expect(expected.spec).toMatchObject(pxcPayload.spec);
+  expect(expected.status.size).toBe(6);
+
+  pxcPayload = expected
+  delete pxcPayload["status"]
+
+  pxcPayload.spec.loadBalancer.type = "LoadBalancer"
+  delete pxcPayload.metadata['finalizers']
+
+  // Update PXC cluster
+
+  let updatedPXCCluster = await request.put(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`, {data: pxcPayload});
+  expect(updatedPXCCluster.ok()).toBeTruthy();
+
+  pxcCluster = await request.get(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  expect (pxcCluster.ok()).toBeTruthy();
+
+  expected = (await pxcCluster.json());
+
+  expect((await updatedPXCCluster.json()).spec.loadBalancer.type).toBe("LoadBalancer");
 
   await request.delete(`/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
 
