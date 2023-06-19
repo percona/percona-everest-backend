da4db29400246bca1575da4a75ca4b7960cdd5ae
Andrew Minkin <andrew.minkin@percona.com>
2023-06-19 23:19:26 +0600

    EVEREST-105 Fixed integration tests

--- api-tests/tests/database-engines.spec.ts
+++ api-tests/tests/database-engines.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-
-test('install and check pxc', async({ request }) => {
-  const kubernetesList = await request.get(`/kubernetes`);
+test('install and check pxc', async ({ request }) => {
+  const kubernetesList = await request.get('/kubernetes');
   const kubernetesId = (await kubernetesList.json())[0].ID;
 
   const enginesList = await request.get(`/kubernetes/${kubernetesId}/database-engines`);
 
   expect(enginesList.ok()).toBeTruthy();
 
-  const engines = (await enginesList.json()).items
-  engines.forEach( (engine) => {
-    if (engine.spec.type == "pxc") {
-      expect(engine.status.status).toBe("installed");
+  const engines = (await enginesList.json()).items;
+  engines.forEach((engine) => {
+    if (engine.spec.type === 'pxc') {
+      expect(engine.status.status).toBe('installed');
     }
-  })
+    if (engine.spec.type === 'psmdb') {
+      expect(engine.status.status).toBe('installed');
+    }
+  });
 });
