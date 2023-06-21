150066099ba89cc085d7c8e3edd84817d1011342
Andrew Minkin <andrew.minkin@percona.com>
2023-06-21 15:39:51 +0600

    EVEREST-105 Formatted code

--- api/helpers.go
+++ api/helpers.go
@@ -5,6 +5,7 @@ import "github.com/percona/percona-everest-backend/model"
 func toKubernetesClusterAPIItem(k model.KubernetesCluster) *KubernetesCluster {
 	return &KubernetesCluster{Name: &k.Name, Id: &k.ID}
 }
+
 func toKubernetesClusterAPIList(list []model.KubernetesCluster) []*KubernetesCluster {
 	items := make([]*KubernetesCluster, len(list))
 	for i := range list {
