fcb68a3825cd7abf1e939cf39bcbd3718d003b74
Michal Kralik <michal.kralik@percona.com>
2023-07-26 10:07:44 +0200

    Example api design

--- api/database_cluster.go
+++ api/database_cluster.go
@@ -1,10 +1,41 @@
 package api //nolint:dupl
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/AlekSi/pointer"
+	"github.com/labstack/echo/v4"
+)
 
 // CreateDatabaseCluster creates a new db cluster inside the given k8s cluster.
 func (e *EverestServer) CreateDatabaseCluster(ctx echo.Context, kubernetesID string) error {
-	return e.proxyKubernetes(ctx, kubernetesID, "")
+	var params CreateDatabaseClusterJSONRequestBody
+	if err := ctx.Bind(&params); err != nil {
+		return ctx.JSON(http.StatusBadRequest, Error{Message: pointer.ToString(err.Error())})
+	}
+
+	// Here we save/process any Everest related fields
+	e.storage.SaveNotificationConfig(ctx, kubernetesID, params.Notification)
+
+	// Apply e.g. resource limits
+	if e.limits.IsOverTeamCPULimit(params.Spec.Engine.Resources) {
+		return ctx.JSON(http.StatusBadRequest, Error{Message: pointer.ToString("CPU limits reached for your team")})
+	}
+
+	// Talk to k8s
+	e.k8s.applyObject(everestv1alpha.DatabaseCluster{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "everest.percona.com/v1alpha1",
+			Kind:       "DatabaseCluster",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   params.Identifier,
+			Labels: params.Labels,
+		},
+		Spec: params.Spec,
+	})
+
+	return ctx.NoContent(200)
 }
 
 // ListDatabaseClusters List of the created database clusters on the specified kubernetes cluster.
