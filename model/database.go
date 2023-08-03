f0f4100ed3ed609111f2cf9862ee6cd283c202b6
Andrew Minkin <andrew.minkin@percona.com>
2023-08-03 15:26:14 +0600

    EVEREST-107 fixed linter

--- model/database.go
+++ model/database.go
@@ -12,6 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 // Package model provides structures and methods to operate databases and another storages.
 // Migration methods, queries, DB connection initialization and another related stuff should be placed here.
 package model
