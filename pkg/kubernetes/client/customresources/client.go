c2a6a9ae7938d189ce13717641531abaf942104d
Michal Kralik <michal.kralik@percona.com>
2023-08-24 08:30:39 +0200

    Clean up typos (#125)

--- pkg/kubernetes/client/customresources/client.go
+++ pkg/kubernetes/client/customresources/client.go
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package customresouces provides methods to work with custom everest k8s resources.
-package customresouces
+// Package customresources provides methods to work with custom everest k8s resources.
+package customresources
 
 import (
 	"sync"
