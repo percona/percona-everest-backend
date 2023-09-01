0f725221ce995d15bbbe730d18ab5651c8fd5ee3
Michal Kralik <michal.kralik@percona.com>
2023-09-01 13:00:05 +0200

    EVEREST-250: redesign db cluster restore (#128)
    Co-authored-by: Oksana Grishchenko <oksana.grishchenko@percona.com>

--- pkg/kubernetes/backup_storage.go
+++ pkg/kubernetes/backup_storage.go
@@ -9,7 +9,7 @@ import (
 
 // IsBackupStorageConfigInUse returns true if the backup storage is in use
 // by the provided Kubernetes cluster.
-func IsBackupStorageConfigInUse(ctx context.Context, name string, kubeClient *Kubernetes) (bool, error) {
+func IsBackupStorageConfigInUse(ctx context.Context, name string, kubeClient *Kubernetes) (bool, error) { //nolint:cyclop
 	dbs, err := kubeClient.ListDatabaseClusters(ctx)
 	if err != nil {
 		return false, errors.Wrap(err, "could not list database clusters in Kubernetes")
@@ -33,14 +33,25 @@ func IsBackupStorageConfigInUse(ctx context.Context, name string, kubeClient *Ku
 		}
 	}
 
+	restores, err := kubeClient.ListDatabaseClusterRestores(ctx)
+	if err != nil {
+		return false, errors.Wrap(err, "could not list database cluster restores in Kubernetes")
+	}
+
+	for _, restore := range restores.Items {
+		if restore.Spec.DataSource.BackupSource != nil && restore.Spec.DataSource.BackupSource.BackupStorageName == name {
+			return true, nil
+		}
+	}
+
 	return false, nil
 }
 
 // BackupStorageNamesFromDBCluster returns a map of backup storage names used by a DB cluster.
 func BackupStorageNamesFromDBCluster(db *everestv1alpha1.DatabaseCluster) map[string]struct{} {
 	names := make(map[string]struct{})
-	if db.Spec.DataSource != nil && db.Spec.DataSource.BackupStorageName != "" {
-		names[db.Spec.DataSource.BackupStorageName] = struct{}{}
+	if db.Spec.DataSource != nil && db.Spec.DataSource.BackupSource != nil && db.Spec.DataSource.BackupSource.BackupStorageName != "" {
+		names[db.Spec.DataSource.BackupSource.BackupStorageName] = struct{}{}
 	}
 
 	for _, schedule := range db.Spec.Backup.Schedules {
