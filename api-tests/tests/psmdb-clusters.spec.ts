a8cc65fccb94f6df41f323257c4b5a30ce89c1c1
Michal Kralik <michal.kralik@percona.com>
2023-09-04 15:03:46 +0200

    Fix tests

--- api-tests/tests/psmdb-clusters.spec.ts
+++ api-tests/tests/psmdb-clusters.spec.ts
@@ -12,30 +12,30 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { test, expect } from '@fixtures';
+import { test, expect } from '@fixtures'
 
-let kubernetesId;
-let recommendedVersion;
+let kubernetesId
+let recommendedVersion
 
 test.beforeAll(async ({ request }) => {
-  const kubernetesList = await request.get('/v1/kubernetes');
+  const kubernetesList = await request.get('/v1/kubernetes')
 
-  kubernetesId = (await kubernetesList.json())[0].id;
+  kubernetesId = (await kubernetesList.json())[0].id
 
-  const engineResponse = await request.get(`/v1/kubernetes/${kubernetesId}/database-engines/percona-server-mongodb-operator`);
-  const availableVersions = (await engineResponse.json()).status.availableVersions.engine;
+  const engineResponse = await request.get(`/v1/kubernetes/${kubernetesId}/database-engines/percona-server-mongodb-operator`)
+  const availableVersions = (await engineResponse.json()).status.availableVersions.engine
 
   for (const k in availableVersions) {
     if (availableVersions[k].status === 'recommended' && k.startsWith('6')) {
-      recommendedVersion = k;
+      recommendedVersion = k
     }
   }
 
-  expect(recommendedVersion).not.toBe('');
-});
+  expect(recommendedVersion).not.toBe('')
+})
 
 test('create/edit/delete single node psmdb cluster', async ({ request, page }) => {
-  const clusterName = 'test-psmdb-cluster';
+  const clusterName = 'test-psmdb-cluster'
   const psmdbPayload = {
     apiVersion: 'everest.percona.com/v1alpha1',
     kind: 'DatabaseCluster',
@@ -63,61 +63,61 @@ test('create/edit/delete single node psmdb cluster', async ({ request, page }) =
         },
       },
     },
-  };
+  }
 
   await request.post(`/v1/kubernetes/${kubernetesId}/database-clusters`, {
     data: psmdbPayload,
-  });
+  })
   for (let i = 0; i < 15; i++) {
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(1000)
 
-    const psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+    const psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
 
-    expect(psmdbCluster.ok()).toBeTruthy();
+    expect(psmdbCluster.ok()).toBeTruthy()
 
-    const result = (await psmdbCluster.json());
+    const result = (await psmdbCluster.json())
 
     if (typeof result.status === 'undefined' || typeof result.status.size === 'undefined') {
-      continue;
+      continue
     }
 
-    expect(result.metadata.name).toBe(clusterName);
-    expect(result.spec).toMatchObject(psmdbPayload.spec);
-    expect(result.status.size).toBe(1);
+    expect(result.metadata.name).toBe(clusterName)
+    expect(result.spec).toMatchObject(psmdbPayload.spec)
+    expect(result.status.size).toBe(1)
 
     // psmdbPayload should be overriden because kubernetes adds data into metadata field
     // and uses metadata.generation during updation. It returns 422 HTTP status code if this field is not present
     //
     // kubectl under the hood merges everything hence the UX is seemless
-    psmdbPayload.spec = result.spec;
-    psmdbPayload.metadata = result.metadata;
-    break;
+    psmdbPayload.spec = result.spec
+    psmdbPayload.metadata = result.metadata
+    break
   }
 
-  psmdbPayload.spec.engine.config = 'operationProfiling:\nmode: slowOp';
+  psmdbPayload.spec.engine.config = 'operationProfiling:\nmode: slowOp'
 
   // Update PSMDB cluster
 
   const updatedPSMDBCluster = await request.put(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`, {
     data: psmdbPayload,
-  });
+  })
 
-  expect(updatedPSMDBCluster.ok()).toBeTruthy();
+  expect(updatedPSMDBCluster.ok()).toBeTruthy()
 
-  let psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  let psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
 
-  expect(psmdbCluster.ok()).toBeTruthy();
+  expect(psmdbCluster.ok()).toBeTruthy()
 
-  expect((await updatedPSMDBCluster.json()).spec.engine.config).toBe(psmdbPayload.spec.engine.config);
+  expect((await updatedPSMDBCluster.json()).spec.engine.config).toBe(psmdbPayload.spec.engine.config)
 
-  await request.delete(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  await request.delete(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
 
-  psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
-  expect(psmdbCluster.status()).toBe(404);
-});
+  psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
+  expect(psmdbCluster.status()).toBe(404)
+})
 
 test('expose psmdb cluster after creation', async ({ request, page }) => {
-  const clusterName = 'exposed-psmdb-cluster';
+  const clusterName = 'exposed-psmdb-cluster'
   const psmdbPayload = {
     apiVersion: 'everest.percona.com/v1alpha1',
     kind: 'DatabaseCluster',
@@ -145,58 +145,58 @@ test('expose psmdb cluster after creation', async ({ request, page }) => {
         },
       },
     },
-  };
+  }
 
   await request.post(`/v1/kubernetes/${kubernetesId}/database-clusters`, {
     data: psmdbPayload,
-  });
+  })
 
   for (let i = 0; i < 15; i++) {
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(1000)
 
-    const psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+    const psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
 
-    expect(psmdbCluster.ok()).toBeTruthy();
+    expect(psmdbCluster.ok()).toBeTruthy()
 
-    const result = (await psmdbCluster.json());
+    const result = (await psmdbCluster.json())
 
     if (typeof result.status === 'undefined' || typeof result.status.size === 'undefined') {
-      continue;
+      continue
     }
 
-    expect(result.metadata.name).toBe(clusterName);
-    expect(result.spec).toMatchObject(psmdbPayload.spec);
-    expect(result.status.size).toBe(6);
+    expect(result.metadata.name).toBe(clusterName)
+    expect(result.spec).toMatchObject(psmdbPayload.spec)
+    expect(result.status.size).toBe(3)
 
-    psmdbPayload.spec = result.spec;
-    psmdbPayload.metadata = result.metadata;
-    break;
+    psmdbPayload.spec = result.spec
+    psmdbPayload.metadata = result.metadata
+    break
   }
 
-  psmdbPayload.spec.proxy.expose.type = 'external';
+  psmdbPayload.spec.proxy.expose.type = 'external'
 
   // Update PSMDB cluster
 
   const updatedPSMDBCluster = await request.put(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`, {
     data: psmdbPayload,
-  });
+  })
 
-  expect(updatedPSMDBCluster.ok()).toBeTruthy();
+  expect(updatedPSMDBCluster.ok()).toBeTruthy()
 
-  let psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  let psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
 
-  expect(psmdbCluster.ok()).toBeTruthy();
+  expect(psmdbCluster.ok()).toBeTruthy()
 
-  expect((await updatedPSMDBCluster.json()).spec.proxy.expose.type).toBe('external');
+  expect((await updatedPSMDBCluster.json()).spec.proxy.expose.type).toBe('external')
 
-  await request.delete(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  await request.delete(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
 
-  psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
-  expect(psmdbCluster.status()).toBe(404);
-});
+  psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
+  expect(psmdbCluster.status()).toBe(404)
+})
 
 test('expose psmdb cluster on EKS to the public internet and scale up', async ({ request, page }) => {
-  const clusterName = 'eks-psmdb-cluster';
+  const clusterName = 'eks-psmdb-cluster'
   const psmdbPayload = {
     apiVersion: 'everest.percona.com/v1alpha1',
     kind: 'DatabaseCluster',
@@ -224,50 +224,50 @@ test('expose psmdb cluster on EKS to the public internet and scale up', async ({
         },
       },
     },
-  };
+  }
 
   await request.post(`/v1/kubernetes/${kubernetesId}/database-clusters`, {
     data: psmdbPayload,
-  });
+  })
   for (let i = 0; i < 15; i++) {
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(2000)
 
-    const psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+    const psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
 
-    expect(psmdbCluster.ok()).toBeTruthy();
+    expect(psmdbCluster.ok()).toBeTruthy()
 
-    const result = (await psmdbCluster.json());
+    const result = (await psmdbCluster.json())
 
     if (typeof result.status === 'undefined' || typeof result.status.size === 'undefined') {
-      continue;
+      continue
     }
 
-    expect(result.metadata.name).toBe(clusterName);
-    expect(result.spec).toMatchObject(psmdbPayload.spec);
-    expect(result.status.size).toBe(6);
+    expect(result.metadata.name).toBe(clusterName)
+    expect(result.spec).toMatchObject(psmdbPayload.spec)
+    expect(result.status.size).toBe(3)
 
-    psmdbPayload.spec = result.spec;
-    psmdbPayload.metadata = result.metadata;
-    break;
+    psmdbPayload.spec = result.spec
+    psmdbPayload.metadata = result.metadata
+    break
   }
 
-  psmdbPayload.spec.engine.replicas = 5;
+  psmdbPayload.spec.engine.replicas = 5
 
   // Update PSMDB cluster
 
   const updatedPSMDBCluster = await request.put(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`, {
     data: psmdbPayload,
-  });
+  })
 
-  expect(updatedPSMDBCluster.ok()).toBeTruthy();
+  expect(updatedPSMDBCluster.ok()).toBeTruthy()
 
-  let psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
+  let psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
 
-  expect(psmdbCluster.ok()).toBeTruthy();
+  expect(psmdbCluster.ok()).toBeTruthy()
 
-  await request.delete(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
-  await page.waitForTimeout(1000);
+  await request.delete(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
+  await page.waitForTimeout(1000)
 
-  psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`);
-  expect(psmdbCluster.status()).toBe(404);
-});
+  psmdbCluster = await request.get(`/v1/kubernetes/${kubernetesId}/database-clusters/${clusterName}`)
+  expect(psmdbCluster.status()).toBe(404)
+})
