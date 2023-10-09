104d52e6c5958eca252fb15f6823a4ad8b95d37c
Andrew Minkin <andrew.minkin@percona.com>
2023-10-09 19:30:32 +0600

    EVEREST-301 Clear message for failed HEAD requests to s3 (#225)

--- api/validation.go
+++ api/validation.go
@@ -197,7 +197,7 @@ func s3Access(l *zap.SugaredLogger, endpoint *string, accessKey, secretKey, buck
 	})
 	if err != nil {
 		l.Error(err)
-		return errors.New("could not issue head request to S3 bucket")
+		return errors.New("unable to connect to s3. Check your credentials")
 	}
 
 	testKey := "everest-write-test"
