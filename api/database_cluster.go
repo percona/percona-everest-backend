f6b808f9c9b21db9a4516137507f74434f808853
Andrew Minkin <andrew.minkin@percona.com>
2023-10-26 19:07:15 +0600

    EVEREST-574 Drop dead code

--- api/database_cluster.go
+++ api/database_cluster.go
@@ -39,32 +39,6 @@ func (e *EverestServer) CreateDatabaseCluster(ctx echo.Context, kubernetesID str
 		return ctx.JSON(http.StatusBadRequest, Error{Message: pointer.ToString(err.Error())})
 	}
 
-	//backupNames := backupStorageNamesFrom(dbc)
-	//err = e.createK8SBackupStorages(ctx.Request().Context(), kubeClient, backupNames)
-	//if err != nil {
-	//	e.l.Error(err)
-	//	return ctx.JSON(http.StatusInternalServerError, Error{
-	//		Message: pointer.ToString("Could not create BackupStorage"),
-	//	})
-	//}
-
-	// if monitoringName := monitoringNameFrom(dbc); monitoringName != "" {
-	//	i, err := e.storage.GetMonitoringInstance(monitoringName)
-	//	if err != nil {
-	//		return ctx.JSON(http.StatusBadRequest, Error{
-	//			Message: pointer.ToString("Could not find monitoring instance"),
-	//		})
-	//	}
-
-	//	err = kubeClient.EnsureConfigExists(ctx.Request().Context(), i, e.secretsStorage.GetSecret)
-	//	if err != nil {
-	//		e.l.Error(err)
-	//		return ctx.JSON(http.StatusBadRequest, Error{
-	//			Message: pointer.ToString("Could not create monitoring config in Kubernetes"),
-	//		})
-	//	}
-	//}
-
 	return e.proxyKubernetes(ctx, kubernetesID, "")
 }
 
@@ -75,35 +49,8 @@ func (e *EverestServer) ListDatabaseClusters(ctx echo.Context, kubernetesID stri
 
 // DeleteDatabaseCluster deletes a database cluster on the specified kubernetes cluster.
 func (e *EverestServer) DeleteDatabaseCluster(ctx echo.Context, kubernetesID string, name string) error {
-	//db, err := e.kubeClient.GetDatabaseCluster(ctx.Request().Context(), name)
-	//if err != nil {
-	//	e.l.Error(err)
-	//	return ctx.JSON(http.StatusInternalServerError, Error{
-	//		Message: pointer.ToString("Could not get database cluster"),
-	//	})
-	//}
 
-	proxyErr := e.proxyKubernetes(ctx, kubernetesID, name)
-	if proxyErr != nil {
-		return proxyErr
-	}
-
-	// At this point the proxy already sent a response to the API user.
-	// We check if the response was successful to continue with cleanup.
-	if ctx.Response().Status >= http.StatusMultipleChoices {
-		return nil
-	}
-
-	// names := kubernetes.BackupStorageNamesFromDBCluster(db)
-	// e.waitGroup.Add(1)
-	// go e.deleteK8SBackupStorages(context.Background(), kubeClient, names)
-
-	//if db.Spec.Monitoring != nil && db.Spec.Monitoring.MonitoringConfigName != "" {
-	//	e.waitGroup.Add(1)
-	//	go e.deleteK8SMonitoringConfig(context.Background(), kubeClient, db.Spec.Monitoring.MonitoringConfigName)
-	//}
-
-	return nil
+	return e.proxyKubernetes(ctx, kubernetesID, name)
 }
 
 // GetDatabaseCluster retrieves the specified database cluster on the specified kubernetes cluster.
@@ -132,12 +79,6 @@ func (e *EverestServer) UpdateDatabaseCluster(ctx echo.Context, kubernetesID str
 	if err := validateDatabaseClusterOnUpdate(dbc, oldDB); err != nil {
 		return ctx.JSON(http.StatusBadRequest, Error{Message: pointer.ToString(err.Error())})
 	}
-	//newMonitoringName := monitoringNameFrom(dbc)
-	//newBackupNames := backupStorageNamesFrom(dbc)
-	//err = e.createResources(ctx.Request().Context(), oldDB, kubeClient, newMonitoringName, newBackupNames)
-	//if err != nil {
-	//	return ctx.JSON(http.StatusInternalServerError, Error{Message: pointer.ToString(err.Error())})
-	//}
 
 	proxyErr := e.proxyKubernetes(ctx, kubernetesID, name)
 	if proxyErr != nil {
@@ -149,11 +90,6 @@ func (e *EverestServer) UpdateDatabaseCluster(ctx echo.Context, kubernetesID str
 	if ctx.Response().Status >= http.StatusMultipleChoices {
 		return nil
 	}
-	// e.waitGroup.Add(1)
-	// go e.deleteBackupStoragesOnUpdate(context.Background(), kubeClient, oldDB, newBackupNames)
-	// e.waitGroup.Add(1)
-	// go e.deleteMonitoringInstanceOnUpdate(context.Background(), kubeClient, oldDB, newMonitoringName)
-
 	return nil
 }
 
@@ -185,63 +121,4 @@ func (e *EverestServer) GetDatabaseClusterCredentials(ctx echo.Context, kubernet
 	}
 
 	return ctx.JSON(http.StatusOK, response)
-}
-
-func backupStorageNamesFrom(dbc *DatabaseCluster) map[string]struct{} {
-	names := make(map[string]struct{})
-	if dbc.Spec == nil {
-		return names
-	}
-
-	if dbc.Spec.DataSource != nil && dbc.Spec.DataSource.BackupSource != nil {
-		names[dbc.Spec.DataSource.BackupSource.BackupStorageName] = struct{}{}
-	}
-
-	if dbc.Spec.Backup == nil || dbc.Spec.Backup.Schedules == nil {
-		return names
-	}
-	for _, schedule := range *dbc.Spec.Backup.Schedules {
-		names[schedule.BackupStorageName] = struct{}{}
-	}
-
-	return names
-}
-
-func monitoringNameFrom(db *DatabaseCluster) string {
-	if db.Spec == nil {
-		return ""
-	}
-
-	if db.Spec.Monitoring == nil {
-		return ""
-	}
-	if db.Spec.Monitoring.MonitoringConfigName == nil {
-		return ""
-	}
-
-	return *db.Spec.Monitoring.MonitoringConfigName
-}
-
-func withBackupStorageNamesFromDBCluster(existing map[string]struct{}, dbc everestv1alpha1.DatabaseCluster) map[string]struct{} {
-	if dbc.Spec.DataSource != nil && dbc.Spec.DataSource.BackupSource != nil && dbc.Spec.DataSource.BackupSource.BackupStorageName != "" {
-		existing[dbc.Spec.DataSource.BackupSource.BackupStorageName] = struct{}{}
-	}
-
-	for _, schedule := range dbc.Spec.Backup.Schedules {
-		if schedule.BackupStorageName != "" {
-			existing[schedule.BackupStorageName] = struct{}{}
-		}
-	}
-
-	return existing
-}
-
-func uniqueKeys(source, target map[string]struct{}) map[string]struct{} {
-	keysNotInSource := make(map[string]struct{}, len(target))
-	for key := range target {
-		if _, exists := source[key]; !exists {
-			keysNotInSource[key] = struct{}{}
-		}
-	}
-	return keysNotInSource
 }
