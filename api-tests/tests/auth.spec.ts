06f0c38fe2ed2387da7a30cb8d5d8518ef57af1c
Michal Kralik <michal.kralik@percona.com>
2023-11-23 21:30:27 +0100

    EVEREST-594: protect API with a password

---
+++ api-tests/tests/auth.spec.ts
@@ -0,0 +1,75 @@
+// percona-everest-backend
+// Copyright (C) 2023 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { expect, test } from '@fixtures'
+
+test('auth header fails with invalid password', async ({ request }) => {
+  const version = await request.get('/v1/version', {
+    headers: {
+      Authorization: 'Bearer 123',
+    },
+  })
+  expect(version.status()).toEqual(401)
+})
+
+test('auth header fails with no content', async ({ request }) => {
+  const version = await request.get('/v1/version', {
+    headers: {
+      Authorization: '',
+    },
+  })
+  expect(version.status()).toEqual(401)
+})
+
+test('auth cookie fails with invalid password', async ({ browser }) => {
+  const ctx = await browser.newContext()
+  await ctx.addCookies([{name: 'everest_token', value: '123', url: 'http://127.0.0.1:8080'}])
+
+  const request = ctx.request
+
+  const version = await request.get('/v1/version', {
+    headers: {
+      Authorization: '',
+    },
+  })
+  expect(version.status()).toEqual(401)
+})
+
+test('auth header is preferred over cookie', async ({ browser }) => {
+  const ctx = await browser.newContext()
+  await ctx.addCookies([{name: 'everest_token', value: '123', url: 'http://127.0.0.1:8080'}])
+
+  const request = ctx.request
+
+  const version = await request.get('/v1/version')
+  expect(version.ok()).toBeTruthy()
+})
+
+test('auth cookie works with a valid password', async ({ browser }) => {
+  const ctx = await browser.newContext()
+  await ctx.addCookies([{
+    name: 'everest_token',
+    value: process.env.API_TOKEN,
+    url: 'http://127.0.0.1:8080',
+  }])
+
+  const request = ctx.request
+
+  const version = await request.get('/v1/version', {
+    headers: {
+      Authorization: '',
+    },
+  })
+  expect(version.ok()).toBeTruthy()
+})
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/percona/everest-operator v0.4.1-0.20231106102613-c61926d6c066
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/zap v1.26.0
+	golang.org/x/crypto v0.15.0
 	k8s.io/api v0.28.3
 	k8s.io/apimachinery v0.28.3
 	k8s.io/cli-runtime v0.28.3
@@ -69,13 +70,12 @@ require (
 	github.com/xlab/treeprint v1.2.0 // indirect
 	go.starlark.net v0.0.0-20230525235612-a134d8f9ddca // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.12.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/term v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
