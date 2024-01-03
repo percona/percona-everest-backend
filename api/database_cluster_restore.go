f4c8ffafc957f86a033ab661e3ed30b8f19de050
Diogo Recharte <diogo.recharte@percona.com>
2024-01-03 23:01:42 +0000

    EVEREST-664 Improve error message (#380)

--- api/database_cluster_restore.go
+++ api/database_cluster_restore.go
@@ -71,7 +71,7 @@ func (e *EverestServer) CreateDatabaseClusterRestore(ctx echo.Context) error {
 	if dbCluster.Status.Status == everestv1alpha1.AppStateRestoring {
 		e.l.Error("failed creating restore because another one is in progress")
 		return ctx.JSON(http.StatusBadRequest, Error{
-			Message: pointer.ToString("Another restore is in progress"),
+			Message: pointer.ToString("Another restore process for this DB cluster is currently in progress. Wait for its completion before initiating another."),
 		})
 	}
 
