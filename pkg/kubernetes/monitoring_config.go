f46cd6fd153006130121b276df91eceb1ae92057
Diogo Recharte <diogo.recharte@percona.com>
2024-02-06 14:28:23 +0000

    EVEREST-496 remove check for VMAgent use of monitoring config
    With the introduction of VMAgent CR reconciliation in everest-operator
    we no longer need to block the deletion of MonitoringConfigs if VMAgent
    is using them.

--- pkg/kubernetes/monitoring_config.go
+++ pkg/kubernetes/monitoring_config.go
@@ -18,10 +18,8 @@ package kubernetes
 
 import (
 	"context"
-	"errors"
 
 	everestv1alpha1 "github.com/percona/everest-operator/api/v1alpha1"
-	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -61,13 +59,6 @@ func (k *Kubernetes) IsMonitoringConfigUsed(ctx context.Context, namespace, moni
 	if err != nil {
 		return false, err
 	}
-	inUse, err := k.isMonitoringConfigUsedByVMAgent(ctx, monitoringConfigName)
-	if err != nil {
-		return false, err
-	}
-	if inUse {
-		return true, nil
-	}
 	options := metav1.ListOptions{
 		LabelSelector: metav1.FormatLabelSelector(&metav1.LabelSelector{
 			MatchLabels: map[string]string{
@@ -82,37 +73,6 @@ func (k *Kubernetes) IsMonitoringConfigUsed(ctx context.Context, namespace, moni
 	if len(list.Items) > 0 {
 		return true, nil
 	}
-	return false, nil
-}
-
-func (k *Kubernetes) isMonitoringConfigUsedByVMAgent(ctx context.Context, name string) (bool, error) {
-	vmAgents, err := k.ListVMAgents()
-	if err != nil {
-		if errors.Is(err, &meta.NoKindMatchError{}) {
-			return false, nil
-		}
-		return false, errors.Join(err, errors.New("could not list VM agents in Kubernetes"))
-	}
-	secretNames := make([]string, 0, len(vmAgents.Items))
-
-	for _, vm := range vmAgents.Items {
-		vm := vm
-		secretNames = append(secretNames, k.SecretNamesFromVMAgent(&vm)...)
-	}
-
-	for _, secretName := range secretNames {
-		mcs, err := k.GetMonitoringConfigsBySecretName(ctx, secretName)
-		if err != nil {
-			return false, err
-		}
-
-		for _, mc := range mcs {
-			if mc.Name == name {
-				return true, nil
-			}
-		}
-	}
-
 	return false, nil
 }
 
